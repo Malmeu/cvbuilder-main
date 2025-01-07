c6f7fbcddc35cf0e6ef3c626ddb1535e11075e74
Malmeu <darkvid3r@gmail.com>
2025-01-07 18:56:38 +0100

    bug vercel

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@
 
 import Link from 'next/link'
 import { motion } from 'framer-motion'
-import { ArrowRight } from 'lucide-react'
-import Image from 'next/image'
 
 export default function Home() {
   return (
